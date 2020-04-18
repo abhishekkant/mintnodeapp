4d2b4b377696ac343cdb66a1e49ad21166e5f068
Abhishek Kant <abhishek@abhishekkant.net>
2020-04-18 21:37:35 +0530

    Change in welcome message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Namaste India ' + process.env.USER);
+  res.send('Namaste India via DevOps ' + process.env.USER);
 });
 
 app.listen(PORT, HOST);
